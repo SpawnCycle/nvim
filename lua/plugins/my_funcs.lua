ca2d7a7f6fd21a893d0ab0f9df6e9317a0713476
SpawnCycle <joloman386@gmail.com>
2025-07-15 11:43:18 +0200

    init

---
+++ lua/plugins/my_funcs.lua
@@ -0,0 +1,87 @@
+local whichkey = require "which-key"
+
+-- I am a 2 space tab person
+vim.tabstop = 2
+vim.shiftwidth = 2
+
+if vim.g.neovide then
+  vim.g.gui_font_default_size = 11
+  vim.g.gui_font_size = vim.g.gui_font_default_size
+  vim.g.gui_font_face = "CaskaydiaCove Nerd Font Mono"
+
+  RefreshGuiFont = function() vim.opt.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size) end
+
+  ResizeGuiFont = function(delta)
+    vim.g.gui_font_size = vim.g.gui_font_size + delta
+    RefreshGuiFont()
+  end
+
+  ResetGuiFont = function()
+    vim.g.gui_font_size = vim.g.gui_font_default_size
+    RefreshGuiFont()
+  end
+
+  -- Call function on startup to set default value
+  ResetGuiFont()
+
+  -- Keymaps
+
+  local opts = { noremap = true, silent = true }
+
+  -- stylua: ignore start
+  vim.keymap.set({ "n", "i" }, "<C-+>",  function() ResizeGuiFont(1)  end, opts)
+  vim.keymap.set({ "n", "i" }, "<C-->",  function() ResizeGuiFont(-1) end, opts)
+  vim.keymap.set({ "n", "i" }, "<C-BS>", function() ResetGuiFont()    end, opts)
+  -- stylua: ignore end
+end
+
+-- description for whichkey
+-- stylua: ignore
+require("which-key").add {
+  mode = { "n" },
+  { "<leader>rn", desc = "Renames variables using treesitter", expr = true, },
+  { "<leader>E",  desc = "Open cwd with oil.nvim", function() vim.cmd "Oil ." end, },
+}
+-- the actual increname function
+vim.keymap.set("n", "<leader>rn", function() return ":IncRename " .. vim.fn.expand "<cword>" end, { expr = true })
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
+
+require("astrolsp").setup {
+  features = {
+    signature_help = true,
+  },
+}
+
+-- align.nvim
+-- stylua: ignore
+whichkey.add {
+  mode = { "v" },
+  { "<leader>a", group = "Align" },
+  { "<leader>aa", function() require("align").align_to_char { length = 1 }                      end, desc = "Align to 1 character" },
+  { "<leader>ad", function() require("align").align_to_char { length = 1, preview = true }      end, desc = "Align to 1 character with preview", },
+  { "<leader>aw", function() require("align").align_to_string { preview = true, regex = false } end, desc = "Align to string", },
+  { "<leader>ar", function() require("align").align_to_string { preview = true, regex = true }  end, desc = "Align to string with regex", },
+}
+
+-- stylua: ignore
+whichkey.add {
+  mode = { "v" },
+  { "<leader>s", group = "Silicon" },
+  { "<leader>sc", function() require("nvim-silicon").shoot() end, desc = "Copy code screenshot to clipboard" },
+  { "<leader>sf", function() require("nvim-silicon").file()  end, desc = "Save code screenshot as file" },
+  { "<leader>ss", function() require("nvim-silicon").clip()  end, desc = "Create code screenshot" },
+}
+
+return {}
---
+++ lua/plugins/my_stuff.lua
@@ -0,0 +1,95 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- transparent = true,
+      -- styles = {
+      --   sidebars = "transparent",
+      --   floats = "transparent"
+      -- }
+    },
+  },
+  {
+    "AstroNvim/astroui",
+    opts = {
+      colorscheme = "tokyonight-storm",
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    opts = {
+      window = {
+        position = "right",
+        width = 30,
+      },
+    },
+  },
+  {
+    "NoahTheDuke/vim-just",
+    ft = { "just" },
+  },
+  {
+    "smjonas/inc-rename.nvim",
+    config = function() require("inc_rename").setup() end,
+  },
+  {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    opts = {
+      theme = "Dracula",
+    },
+  },
+  { "echasnovski/mini.icons", version = "*" },
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      columns = {
+        "icon",
+        "permissions",
+        "size",
+        "msize",
+      },
+      keymaps = {
+        ["<C-s>"] = false,
+      },
+      watch_for_changes = true,
+    },
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+  {
+    "https://git.sr.ht/~swaits/scratch.nvim",
+    lazy = true,
+    keys = {
+      { "<leader>bt", "<cmd>Scratch<cr>", desc = "Scratch Buffer", mode = "n" },
+      { "<leader>bT", "<cmd>ScratchSplit<cr>", desc = "Scratch Buffer (split)", mode = "n" },
+    },
+    cmd = {
+      "Scratch",
+      "ScratchSplit",
+    },
+    opts = {},
+  },
+  {
+    "Vonr/align.nvim", -- mini.align? don't quite like the defaults
+    branch = "v2",
+    lazy = true,
+    init = function() end,
+  },
+}
